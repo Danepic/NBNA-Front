e7c87c28de846a15042cc1d64d846bce965c99e8
Danilo <danilokhpp@hotmail.com>
2020-04-07 00:38:08 -0300

    Configurando firebase no projeto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:   {
+    apiKey: "AIzaSyA0aaJuwcjEftTeyBoKpW_6T_DOhtfqVrs",
+    authDomain: "nbna-alpha.firebaseapp.com",
+    databaseURL: "https://nbna-alpha.firebaseio.com",
+    projectId: "nbna-alpha",
+    storageBucket: "nbna-alpha.appspot.com",
+    messagingSenderId: "246667412010",
+    appId: "1:246667412010:web:48687eeec7e47d24"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:   {
+    apiKey: "AIzaSyA0aaJuwcjEftTeyBoKpW_6T_DOhtfqVrs",
+    authDomain: "nbna-alpha.firebaseapp.com",
+    databaseURL: "https://nbna-alpha.firebaseio.com",
+    projectId: "nbna-alpha",
+    storageBucket: "nbna-alpha.appspot.com",
+    messagingSenderId: "246667412010",
+    appId: "1:246667412010:web:48687eeec7e47d24"
+  }
 };
 
 /*
